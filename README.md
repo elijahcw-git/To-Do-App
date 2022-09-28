eae18bc52ef68ab390d79c7431a853b0e26f7a8a
elijahcw-git <85442180+elijahcw-git@users.noreply.github.com>
2022-09-27 20:32:14 -0500

    Styled, refactored, and tested

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # To Do Application
+
+### Task 5 in the Software Development Mastermind Program
+
+#### You can test this program [here]()
+
+## Summary
+
+A so-called "right of passage" for new software developers is to create the ever-famous to-do list. This project intentionally avoided significant styling efforts. The to-do list includes vanilla Javascript, and HTML/CSS. In Javascript, several DOM manipulations were used in order to provide functionality, and to provide more practice during the SDMM program. Some challenges during this development process included properly addressing attributes of specific DOM elements, and using the correct selector criteria. 
+
+## Author
+
+### Elijah C Walker - Systems Engineer/MS Computer Science Candidate/US Navy Veteran
--- script.js
+++ script.js
@@ -9,50 +9,40 @@ submitBtn.addEventListener('click', (e) =>{
 
 function addToDoItem(){
     //create useable node;
-    let node = document.createElement('li');
+    const node = document.createElement('li');
     node.setAttribute('class', 'toDoEntry');
+    createCheckBox(node);
+    getTextInput(node);
+    createDeleteButton(node);      
+}
 
-    //create checkbox
+function getTextInput(node){
+    const nodeText = document.createTextNode(textInput.value);
+    const toDoEntry = document.querySelector('.toDoEntry')
+    node.appendChild(nodeText);
+    toDoList.appendChild(node);    
+    textInput.value = '';
+}
 
-    let checkBox = document.createElement("input");
+function createCheckBox(node){
+    const  checkBox = document.createElement("input");
     checkBox.setAttribute('type', 'checkbox');
     checkBox.setAttribute('class', 'checkBox');
     node.appendChild(checkBox);
-    
-    
-    //create text entry
-
-    let nodeText = document.createTextNode(textInput.value);
-    let toDoEntry = document.querySelector('.toDoEntry')
-    node.appendChild(nodeText);
-    toDoList.appendChild(node);
-    
-    //clear input back to placeholder value
-
-    textInput.value = '';
-
-    //create delete Button
-
-    let delBtn = document.createElement('button');
-    delBtn.setAttribute('class', 'delBtn');
-    let delBtnText = document.createTextNode('Delete');
-    delBtn.appendChild(delBtnText);
-    node.appendChild(delBtn);
-
-    //create delete function
-    
-    delBtn.addEventListener('click', (e) =>{
-        e.preventDefault();
-        e.target.parentElement.remove();
-    })
-
-    //create checkbox
-
     checkBox.addEventListener('change', (e) =>{
         if(e.target.checked){
-            node.style.textDecoration = "line-through";
-        } else node.style.textDecoration = "none";
-        
+            e.target.parentElement.style.textDecoration = "line-through";
+        } else e.target.parentElement.style.textDecoration = "none";       
     })
-    
+}
+
+function createDeleteButton(node){
+    const delBtn = document.createElement('button');
+    delBtn.setAttribute('class', 'delBtn');
+    delBtn.innerHTML = '<i class="fa fa-trash-o" style="font-size:24px"></i>';
+    node.appendChild(delBtn);
+    delBtn.addEventListener('click', (e) =>{
+        e.preventDefault();
+        delBtn.parentElement.remove();
+    })
 }
