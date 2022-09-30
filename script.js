86a80067909427c8b9e634da62cc85969c195a19
elijahcw-git <85442180+elijahcw-git@users.noreply.github.com>
2022-09-30 18:05:08 -0500

    refactored, fixed input validation and deletion bug

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
-const textInput = document.querySelector('.textInput');
-const submitBtn = document.querySelector('.submitBtn');
-const toDoList = document.querySelector('.toDoList');
+const textInput = document.querySelector(".textInput");
+const submitBtn = document.querySelector(".submitBtn");
+const toDoList = document.querySelector(".toDoList");
 let checked = false;
 
-submitBtn.addEventListener('click', (e) =>{  
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (textInput.value.trim().length === 0) return;
+  addToDoItem();
+});
+
+function addToDoItem() {
+  const toDoItem = document.createElement("li");
+  toDoItem.setAttribute("class", "toDoEntry");
+  createCheckBox(toDoItem);
+  getTextInput(toDoItem);
+  createDeleteButton(toDoItem);
+}
+
+function getTextInput(toDoItem) {
+  const nodeText = document.createTextNode(textInput.value);
+  toDoItem.appendChild(nodeText);
+  toDoList.appendChild(toDoItem);
+  textInput.value = "";
+}
+
+function createCheckBox(toDoItem) {
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  toDoItem.appendChild(checkBox);
+  checkBox.addEventListener("change", (e) => {
+    e.target.checked
+      ? (e.target.parentElement.style.textDecoration = "line-through")
+      : (e.target.parentElement.style.textDecoration = "none");
+  });
+}
+
+function createDeleteButton(toDoItem) {
+  const delBtn = document.createElement("button");
+  delBtn.setAttribute("class", "delBtn");
+  delBtn.innerHTML = '<i class="fa fa-trash-o" style="font-size:24px"></i>';
+  toDoItem.appendChild(delBtn);
+  delBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    addToDoItem();
-})
-
-function addToDoItem(){
-    const node = document.createElement('li');
-    node.setAttribute('class', 'toDoEntry');
-    createCheckBox(node);
-    getTextInput(node);
-    createDeleteButton(node);      
-}
-
-function getTextInput(node){  
-    const nodeText = document.createTextNode(textInput.value);
-    const toDoEntry = document.querySelector('.toDoEntry')
-    node.appendChild(nodeText);
-    toDoList.appendChild(node);    
-    textInput.value = '';
-}
-
-function createCheckBox(node){
-    const  checkBox = document.createElement("input");
-    checkBox.setAttribute('type', 'checkbox');
-    checkBox.setAttribute('class', 'checkBox');
-    node.appendChild(checkBox);
-    checkBox.addEventListener('change', (e) =>{
-        if(e.target.checked){
-            e.target.parentElement.style.textDecoration = "line-through";
-            checked = true;
-        } else {
-            e.target.parentElement.style.textDecoration = "none";
-            checked = false;
-        }       
-    })
-}
-
-function createDeleteButton(node){
-    const delBtn = document.createElement('button');
-    delBtn.setAttribute('class', 'delBtn');
-    delBtn.innerHTML = '<i class="fa fa-trash-o" style="font-size:24px"></i>';
-    node.appendChild(delBtn);
-    delBtn.addEventListener('click', (e) =>{
-        e.preventDefault();    
-        checked ? delBtn.parentElement.remove() : alert('You must check off your to do before removing!') 
-    })
+    e.target.parentElement.previousSibling.previousSibling.checked
+      ? delBtn.parentElement.remove()
+      : alert("You must check off your to do before removing!");
+  });
 }
